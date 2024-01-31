2d0d9087f141bf8f0cb1fdc5ef90179acfdd7287
Marcio de Andrade <gmandrade1@hotmail.com>
2024-01-31 15:27:21 -0400

    ajustos home component criando pagina content e dataFake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    "id":1,
+    "photo": "aaaa",
+    "title": "saiu novo filme",
+    "description": "olá"
+  },
+  {
+    "id":2,
+    "photo": "aaaa",
+    "title": "saiu outro filme",
+    "description": "olá novamente"
+  }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  photoCover:string = "https://th.bing.com/th/id/OIP.v1cPk-6MbE5g0B4_I-BvSQHaE7?rs=1&pid=ImgDetMain"
+  contentTitle:string = "notícia exemplo"
+  contentDescription:string = "bla bla bla "
   constructor() { }
 
   ngOnInit(): void {
